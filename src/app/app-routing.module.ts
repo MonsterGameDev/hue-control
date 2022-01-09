796f0bd571fefb305a7ef45737b02dac23ff3e6e
MonsterGameDev <per@brf.dk>
2022-01-09 13:46:21 +0100

    routes and menu-component init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CreateComponent } from './create/create.component';
+import { GuideComponent } from './guide/guide.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OverviewComponent } from './overview/overview.component';
+import { PlayerComponent } from './player/player.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'player', component: PlayerComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'overview', component: OverviewComponent },
+  { path: 'guide', component: GuideComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
