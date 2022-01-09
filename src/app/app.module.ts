37c8628773654820c5ad7e7553434c28b1513aa6
MonsterGameDev <per@brf.dk>
2022-01-09 13:46:00 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,11 @@ import { ScenesListComponent } from './scenes/scenes-list/scenes-list.component'
 import { SceneItemComponent } from './scenes/scene-item/scene-item.component';
 import { PlayerComponent } from './audio/player/player.component';
 import { EditComponent } from './audio/edit/edit.component';
+import { MenuComponent } from './menu/menu.component';
+import { CreateComponent } from './create/create.component';
+import { OverviewComponent } from './overview/overview.component';
+import { GuideComponent } from './guide/guide.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -31,15 +36,23 @@ import { EditComponent } from './audio/edit/edit.component';
     SceneItemComponent,
     PlayerComponent,
     EditComponent,
+    MenuComponent,
+    CreateComponent,
+    OverviewComponent,
+    GuideComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
