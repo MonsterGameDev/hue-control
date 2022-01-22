b789e794b246430c904a69011ce387642c734857
MonsterGameDev <per@brf.dk>
2022-01-22 23:50:02 +0100

    steps done

--- src/app/flow/steps-list/steps-list.component.ts
+++ src/app/flow/steps-list/steps-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
-import { Step } from 'src/app/+state/flow/flow.interfaces';
-import { selectAllSteps } from 'src/app/+state/flow/flow.selectors';
+import { Flow, FlowState, Step } from 'src/app/+state/flow/flow.interfaces';
+import { selectAllSteps, selectFlow } from 'src/app/+state/flow/flow.selectors';
 
 @Component({
   selector: 'app-steps-list',
@@ -14,16 +14,26 @@ export class StepsListComponent implements OnInit, OnDestroy {
 
   steps!: Step[];
   steps$: Observable<Step[]> = this.store.pipe(select(selectAllSteps));
+  flow$: Observable<FlowState> = this.store.pipe(select(selectFlow));
+  flow?: Flow;
 
   constructor(private store: Store) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subs.push(this.flow$.subscribe((data) => (this.flow = data.flow)));
+  }
 
   ngOnDestroy(): void {
     this.subs.forEach((sub: Subscription) => sub.unsubscribe());
   }
 
   saveblob() {
-    
+    window.open(
+      URL.createObjectURL(
+        new Blob([JSON.stringify(this.flow)], {
+          type: 'application/binary',
+        })
+      )
+    );
   }
 }
