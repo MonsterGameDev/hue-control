73b5aa812796e438eb7c6d849757dd5da75aadec
MonsterGameDev <per@brf.dk>
2022-01-23 20:55:44 +0100

    stylet action menu save-load

--- src/app/save-and-load-flow/save-and-load-flow.component.ts
+++ src/app/save-and-load-flow/save-and-load-flow.component.ts
@@ -1,10 +1,11 @@
-import { JsonpClientBackend } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Flow, FlowState, Step } from '../+state/flow/flow.interfaces';
 import { selectFlow } from '../+state/flow/flow.selectors';
+
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-save-and-load-flow',
@@ -13,6 +14,8 @@ import { selectFlow } from '../+state/flow/flow.selectors';
 })
 export class SaveAndLoadFlowComponent implements OnInit {
   subs: Subscription[] = [];
+  faBars = faBars;
+  faTimes = faTimes;
 
   flow$: Observable<Flow> = this.store.pipe(
     select(selectFlow),
@@ -28,16 +31,14 @@ export class SaveAndLoadFlowComponent implements OnInit {
   constructor(private store: Store) {}
 
   ngOnInit(): void {
-    this.subs.push(this.flow$.subscribe(
-      (flow: Flow) => {
+    this.subs.push(
+      this.flow$.subscribe((flow: Flow) => {
         this.flow = flow;
         this.steps = flow.steps;
         this.hasSteps = flow.steps.length > 0;
         this.flowTitle = flow.projectName;
-
-
-      }
-    ));
+      })
+    );
   }
 
   saveToFile() {}
